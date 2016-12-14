7101120d05652fe4c008447a2ea670ff3fc97caf
Joost De Cock <joost@decock.org>
2016-12-14 15:27:10 +0000

    Changes to paperless theme

--- themes/Paperless/Paperless.php
+++ themes/Paperless/Paperless.php
@@ -33,7 +33,7 @@ class Paperless extends Svg
         $templateDir = $this->getTemplateDir();
         $this->defs = file_get_contents("$templateDir/defs/grid.".$units['out']);
         foreach ($pattern->parts as $key => $part) {
-            if ($part->getRender() === true) {
+            if ($part->getRender() === true && $part->hasPathToRender()) {
                 $id = $part->newId('grid');
                 $this->defs .= "\n".'<pattern id="grid-'.$key.'" xlink:href="grid"></pattern>';
                 $this->addGridToPart($part);
@@ -61,6 +61,7 @@ class Paperless extends Svg
      */
     private function addGridToPart($part, $units = 'metric')
     {
+        if(is_array($part->boundary)) print_r($part);
         $topLeft = $part->boundary->getTopLeft();
         $w = $part->boundary->width;
         $h = $part->boundary->height;

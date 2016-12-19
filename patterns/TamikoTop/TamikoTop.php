c915772645e94b6b025cad8ce46e567b20c5a9a2
Joost De Cock <joost@decock.org>
2016-12-19 16:25:31 +0000

    Tamiko update, fixes #22

--- patterns/TamikoTop/TamikoTop.php
+++ patterns/TamikoTop/TamikoTop.php
@@ -11,6 +11,16 @@ namespace Freesewing\Patterns;
  */
 class TamikoTop extends Pattern
 {
+    /*
+        ____             __ _
+       |  _ \ _ __ __ _ / _| |_
+       | | | | '__/ _` | |_| __|
+       | |_| | | | (_| |  _| |_
+       |____/|_|  \__,_|_|  \__|
+
+      The actual sampling/drafting of the pattern
+    */
+
     /**
      * Generates a draft of the pattern
      *
@@ -25,7 +35,13 @@ class TamikoTop extends Pattern
     public function draft($model)
     {
         $this->sample($model);
-        $this->finalizeTop($model, $this->parts['top']);
+
+        $this->finalizeTop($model);
+
+        if ($this->isPaperless) {
+            $this->paperlessTop($model);
+        }
+
     }
 
     /**
@@ -42,159 +58,7 @@ class TamikoTop extends Pattern
      */
     public function sample($model)
     {
-        $this->draftTop($model, $this->parts['top']);
-    }
-
-    /**
-     * Finalizes the Interfacing Tip
-     *
-     * @param \Freesewing\Model $model The model to finalize the part for
-     * @param \Freesewing\Part $p The part object
-     *
-     * @return void
-     */
-    public function finalizeTop($model, $p)
-    {
-        /* Title */
-        $p->newPoint('titleAnchor', $p->x(42)/2, $p->y(42)/2);
-        $p->addTitle('titleAnchor', 1, $this->t($p->title), '1x '.$this->t('From main fabric')."\n".$this->t('Cut on fold'));
-
-        // Cut-on-fold (cof)
-        $p->newPoint('cofStart', $p->x(1)+40, $p->y(2));
-        $p->newPoint('cofLeft', $p->x('cofStart'), $p->y(5)-30);
-        $p->newPoint('cofEnd', $p->x(42)-40, $p->y(2));
-        $p->newPoint('cofRight', $p->x('cofEnd'), $p->y('cofLeft'));
-        $cof = 'M cofStart L cofLeft L cofRight L cofEnd';
-        $p->newPath('cof', $cof, ['class' => 'grainline']);
-        $p->newTextOnPath('cof', $cof, $this->t('Cut on fold'), ['line-height' => 12, 'class' => 'text-lg fill-fabric text-center', 'dy' => -2]);
-     
-        // Grainline
-        $p->newPoint('grainlineTop', $p->x(1)+10, $p->y('cofLeft')-30);
-        $p->newPoint('grainlineBottom', $p->x(42)-10, $p->y('grainlineTop'));
-        $p->newPath('grainline', 'M grainlineTop L grainlineBottom', ['class' => 'grainline']);
-        $p->newTextOnPath('grainline', 'M grainlineTop L grainlineBottom', $this->t('Grainline'), ['line-height' => 12, 'class' => 'text-lg fill-fabric text-center', 'dy' => -2]);
-
-        // Seam allowance
-        $p->offsetPathString('sa', 'M 3 C 3 6 5 C 7 4 4', 10, 1, ['class' => 'seam-allowance']);
-        
-        // Paperless
-        if ($this->isPaperless) {
-            // Measures
-            $pAttr = ['class' => 'measure-lg'];
-            $hAttr = ['class' => 'stroke-note stroke-sm'];
-
-            // Bring points to the left
-            $p->newPoint(100, $p->x(1), $p->y(23));
-            $p->newPoint(101, $p->x(1), $p->y(22));
-            
-            // Bring points to top
-            $p->newPoint(102, $p->x(22), $p->y(41));
-            $p->newPoint(103, $p->x(23), $p->y(41));
-            
-            // Bring points to bottom
-            $p->newPoint(104, $p->x(10), $p->y(2));
-            $p->newPoint(105, $p->x(5), $p->y(2));
-
-
-            $key = 'toShoulderSeam';
-            $path = 'M 2 L 104';
-            $p->offsetPathString($key, $path, 15, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(2, 104)), $this->textAttr(13));
-
-            $key = 'toArmholeTop';
-            $path = 'M 2 L 3';
-            $p->offsetPathString($key, $path, 30, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(2, 3)), $this->textAttr(28));
-
-            $key = 'toArmholeBottom';
-            $path = 'M 3 L 4';
-            $p->offsetPathString($key, $path, 30, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(3, 4)), $this->textAttr(28));
-
-            $key = 'armholeDepth';
-            $path = 'M 105 L 5';
-            $p->newPath($key, $path, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(105, 5)), $this->textAttr(-2));
-
-            $key = 'armholeLeft';
-            $path = 'M 3 L 105';
-            $p->newPath($key, $path, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(3, 105)), $this->textAttr(-2));
-
-            $key = 'armholeRight';
-            $path = 'M 105 L 4';
-            $p->newPath($key, $path, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(105, 4)), $this->textAttr(-2));
-
-            $key = 'width';
-            $path = 'M 2 L 42';
-            $p->offsetPathString($key, $path, 45, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(2, 42)), $this->textAttr(43));
-
-            $key = 'sideHeight1';
-            $path = 'M 23 L 103';
-            $p->offsetPathString($key, $path, 15, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->deltaY(41, 23)), $this->textAttr(13));
-            
-            $key = 'sideHeight2';
-            $path = 'M 101 L 41';
-            $p->offsetPathString($key, $path, -15, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(41, 101)), $this->textAttr(-17));
-            
-            $key = 'sideHeight3';
-            $path = 'M 1 L 41';
-            $p->offsetPathString($key, $path, -30, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(41, 1)), $this->textAttr(-32));
-            
-            $key = 'sideHeight4';
-            $path = 'M 8 L 1';
-            $p->offsetPathString($key, $path, -30, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(1, 8)), $this->textAttr(-32));
-            
-            $key = 'height';
-            $path = 'M 2 L 41';
-            $p->offsetPathString($key, $path, -45, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(41, 2)), $this->textAttr(-47));
-
-
-            $key = 'down1';
-            $path = 'M 41 L 102';
-            $p->offsetPathString($key, $path, -10, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(41, 102)), $this->textAttr(-12));
-
-            $key = 'down2';
-            $path = 'M 41 L 103';
-            $p->offsetPathString($key, $path, -25, 1, $pAttr);
-            $p->newTextOnPath($key, $path, $this->unit($p->distance(41, 103)), $this->textAttr(-27));
-
-            // Draw an extra hidden line to extend the bounding box
-            // so that the measure text does not get cut off
-            $p->addPoint(106, $p->shift('down2-line-41TO103', 90, 12));
-            $p->newPath($p->newId(), 'M 106 L 41', ['class' => 'hidden']);
-
-            $measureHelpLines = 'M 41 L down2-line-41TO103 '.
-                'M 22 L  down1-line-102TO41 '.
-                'M 23 L down2-line-103TO41 '.
-                'M 41 L height-line-41TO2 '.
-                'M 23 L sideHeight1-line-23TO103 '.
-                'M 22 L sideHeight2-line-101TO41 '.
-                'M 31 sideHeight4-line-1TO8 '.
-                'M 38 L sideHeight4-line-8TO1 '.
-                'M 44 L height-line-2TO41 '.
-                'M 44 L width-line-2TO42 '.
-                'M 3 L toArmholeBottom-line-3TO4 '.
-                'M 4 L toArmholeBottom-line-4TO3 '.
-                'M 42 L width-line-42TO2 '.
-                '';
-            $p->newPath($p->newId(), $measureHelpLines, $hAttr);
-            
-            // Notes
-            $p->addPoint(200, $p->shift(5, -5, 30));
-            $p->addPoint(201, $p->shift(8, -90, 50));
-            $p->newNote('saNote', 200, $this->t("Standard\nseam\nallowance")."\n(".$this->unit(10).')', 1, 30, -3, ['line-height' => 6, 'class' => 'text-lg', 'dy' => -15]);
-            $p->newNote('noSaNote', 201, $this->t("No\nseam\nallowance"), 3, 30, 0, ['line-height' => 6, 'class' => 'text-lg', 'dy' => -5]);
-
-        }
+        $this->draftTop($model);
     }
 
     /**
@@ -203,12 +67,14 @@ class TamikoTop extends Pattern
      * @see \Freesewing\Patterns\TrayvonTie::draftInterfacingTip()
      *
      * @param \Freesewing\Model $model The model to draft for
-     * @param \Freesewing\Part $p The part object
      *
      * @return void
      */
-    public function draftTop($model, $p)
+    public function draftTop($model)
     {
+        /** @var \Freesewing\Part $p */
+        $p = $this->parts['top'];
+
         // Shoulder to shoulder len
         $s2s = $model->m('acrossBack')+50;
         // Armhole depth
@@ -251,9 +117,115 @@ class TamikoTop extends Pattern
         $p->paths['sewline']->setSample(true);
          
     }
+
+    /*
+       _____ _             _ _
+      |  ___(_)_ __   __ _| (_)_______
+      | |_  | | '_ \ / _` | | |_  / _ \
+      |  _| | | | | | (_| | | |/ /  __/
+      |_|   |_|_| |_|\__,_|_|_/___\___|
+
+      Adding titles/logos/seam-allowance/grainline and so on
+    */
+
+    /**
+     * Finalizes the top
+     *
+     * @param \Freesewing\Model $model The model to finalize the part for
+     *
+     * @return void
+     */
+    public function finalizeTop($model)
+    {
+        /** @var \Freesewing\Part $p */
+        $p = $this->parts['top'];
+
+        // Title 
+        $p->newPoint('titleAnchor', $p->x(42)/2, $p->y(42)/2);
+        $p->addTitle('titleAnchor', 1, $this->t($p->title), '1x '.$this->t('From main fabric')."\n".$this->t('Cut on fold'));
+
+        // Cut-on-fold (cof)
+        $p->newPoint('cofStart', $p->x(1)+40, $p->y(2));
+        $p->newPoint('cofEnd', $p->x(42)-40, $p->y(2));
+        $p->newCutOnFold('cofStart','cofEnd',$this->t('Cut on fold'),-95);
+     
+        // Grainline
+        $p->newPoint('grainlineStart', $p->x('cofStart'), $p->y('cofStart')-120);
+        $p->newPoint('grainlineEnd', $p->x('cofEnd'), $p->y('grainlineStart'));
+        $p->newGrainline('grainlineStart','grainlineEnd',$this->t('Grainline'));
+
+        // Logo
+        $p->addPoint('logoAnchor', $p->shift('titleAnchor',90,120));
+        $p->newSnippet('logo', 'logo', 'logoAnchor');
+
+        // Scalebox
+        $p->addPoint('scaleboxAnchor', $p->shift('logoAnchor',-90,20));
+        $p->newSnippet('scalebox', 'scalebox', 'scaleboxAnchor');
+
+        // Seam allowance
+        $p->offsetPathString('sa', 'M 3 C 3 6 5 C 7 4 4', 10, 1, ['class' => 'seam-allowance']);
+    }
+
     
     private function textAttr($dy)
     {
         return ['class' => 'text-lg fill-note text-center', 'dy' => $dy];
+    }
+    
+    /*
+        ____                       _
+       |  _ \ __ _ _ __   ___ _ __| | ___  ___ ___
+       | |_) / _` | '_ \ / _ \ '__| |/ _ \/ __/ __|
+       |  __/ (_| | |_) |  __/ |  | |  __/\__ \__ \
+       |_|   \__,_| .__/ \___|_|  |_|\___||___/___/
+                  |_|
+
+      Instructions for paperless patterns
+    */
+
+    /**
+     * Adds paperless info for the top 
+     *
+     * @param \Freesewing\Model $model The model to draft for
+     *
+     * @return void
+     */
+    public function paperlessTop($model)
+    {
+        /** @var \Freesewing\Part $p */
+        $p = $this->parts['top'];
+
+        // Widths at the bottom
+        $yBase = $p->y(2);
+        $p->newWidthDimension(2,10,$yBase+15);
+        $p->newWidthDimension(2,3,$yBase+30);
+        $p->newWidthDimension(2,4,$yBase+45);
+        $p->newWidthDimension(2,42,$yBase+60);
+
+        // Depth armhole
+        $p->newHeightDimension(3,5,$p->x(5));
+
+        // Heigths at the left
+        $xBase = $p->x(41);
+        $p->newHeightDimension(22,41,$xBase-15);
+        $p->newHeightDimension(31,41,$xBase-30);
+        $p->newHeightDimension(38,31,$xBase-30);
+        $p->newHeightDimension(2,31,$xBase-45);
+        $p->newHeightDimension(42,10,$xBase-15);
+        
+        // Widths at the top
+        $yBase = $p->y(41);
+        $p->newWidthDimension(41,22,$yBase-15);
+        $p->newWidthDimension(41,23,$yBase-30);
+
+        // Extra heigth
+        $p->newHeightDimension(23,41,$p->x(23)+15);
+        
+        // Notes
+        $p->addPoint(200, $p->shift(5, -5, 30));
+        $p->addPoint(201, $p->shift(8, -90, 50));
+        $p->newNote('saNote', 200, $this->t("Standard\nseam\nallowance")."\n(".$this->unit(10).')', 1, 30, -3, ['line-height' => 6, 'class' => 'text-lg', 'dy' => -15]);
+        $p->newNote('noSaNote', 201, $this->t("No\nseam\nallowance"), 3, 30, 0, ['line-height' => 6, 'class' => 'text-lg', 'dy' => -5]);
+        $p->newNote('0.5cm', 31, $this->unit(5).' '.$this->t("from the edge"), 2, 30, 0, ['line-height' => 6, 'class' => 'text-lg', 'dy' => 0]);
     }
 }

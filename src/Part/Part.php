joostdecock/core

commit c6d2b897d7650b2a37947654972ad636b44f4f20

Unit tests overall code coverage is 97.58%. Time to do something else.

src/Part/Part.php
@@ -1069,8 +1069,10 @@ class Part
                 } elseif ($chunk['type'] == 'curve' && $next['type'] == 'curve') {
                     $new = $this->fillPathStackCurveCurveGap($chunk,$next);
                 }
-                if ($new) $stack->replace($chunk, $new);
-                unset($new);
+                if (isset($new)) {
+                    $stack->replace($chunk, $new);
+                    unset($new);
+                }
             }
             ++$count;
         }
@@ -1825,26 +1827,13 @@ class Part
         $point1 = $this->loadPoint($key1);
         $point2 = $this->loadPoint($key2);
         $angle = $this->angle($key1, $key2);
-        // cos is x axis, sin is y axis
-        $deltaX = $distance * abs(cos(deg2rad($angle)));
-        $deltaY = $distance * abs(sin(deg2rad($angle)));
-        if ($point1->getX() < $point2->getX() && $point1->getY() > $point2->getY()) {
-            $x = $point1->getX() + abs($deltaX);
-            $y = $point1->getY() - abs($deltaY);
-        } elseif ($point1->getX() < $point2->getX() && $point1->getY() < $point2->getY()) {
-            $x = $point1->getX() + abs($deltaX);
-            $y = $point1->getY() + abs($deltaY);
-        } elseif ($point1->getX() > $point2->getX() && $point1->getY() > $point2->getY()) {
-            $x = $point1->getX() - abs($deltaX);
-            $y = $point1->getY() - abs($deltaY);
-        } elseif ($point1->getX() > $point2->getX() && $point1->getY() < $point2->getY()) {
-            $x = $point1->getX() - abs($deltaX);
-            $y = $point1->getY() + abs($deltaY);
-        } else {
-            $x = $point1->getX() + $deltaX;
-            $y = $point1->getY() + $deltaY;
-        }
-
+        
+        $deltaX = $distance * cos(deg2rad($angle)) * -1;
+        $deltaY = $distance * sin(deg2rad($angle));
+    
+        $x = $point1->getX() + $deltaX;
+        $y = $point1->getY() + $deltaY;
+    
         return $this->createPoint($x, $y, "Point $key1 shifted towards $key2 by $distance");
     }
 

src/Polynomial/Polynomial.php
@@ -238,6 +238,81 @@ class Polynomial
     }
 
     /**
+     * Gets roots of a quartic polynomial
+     *
+     * @return array $results Array of roots
+     */
+    public function getQuarticRoots()
+    {
+        $results = array();
+
+        if ($this->getDegree() == 4) {
+            $c4 = $this->coefs[4];
+            $c3 = $this->coefs[3] / $c4;
+            $c2 = $this->coefs[2] / $c4;
+            $c1 = $this->coefs[1] / $c4;
+            $c0 = $this->coefs[0] / $c4;
+
+            $resolveRoots = new Polynomial(1, -$c2, $c3*$c1 - 4*$c0, -$c3*$c3*$c0 + 4*$c2*$c0 -$c1*$c1);
+            $resolveRoots = $resolveRoots->getCubicRoots();
+            var_dump($resolveRoots);
+            $y = $resolveRoots[0];
+            $discrim = $c3*$c3/4 - $c2 + $y;
+
+            if (abs($discrim) <= $this->tolerance ) $discrim = 0;
+
+            if ($discrim > 0) {
+                $e = sqrt($discrim);
+                $t1 = 3*$c3*$c3/4 - $e*$e - 2*$c2;
+                $t2 = (4*$c3*$c2 - 8*$c1 - $c3*$c3*$c3) / (4*$e);
+                $plus  = $t1+$t2;
+                $minus = $t1-$t2;
+
+                if (abs($plus)  <= $this->tolerance) $plus  = 0;
+                if (abs($minus) <= $this->tolerance) $minus = 0;
+
+                if ($plus >= 0) {
+                    $f = sqrt($plus);
+
+                    $results[] = -$c3/4 + ($e+$f)/2;
+                    $results[] = -$c3/4 + ($e-$f)/2;
+                }
+                if ($minus >= 0) {
+                    $f = sqrt($minus);
+
+                    $results[] = -$c3/4 + ($f-$e)/2;
+                    $results[] = -$c3/4 - ($f+$e)/2;
+                }
+            } else if ($discrim < 0) {
+                // no roots
+            } else {
+                $t2 = $y*$y - 4*$c0;
+
+                if ($t2 >= -$this->tolerance) {
+                    if ($t2 < 0) $t2 = 0;
+
+                    $t2 = 2*sqrt($t2);
+                    $t1 = 3*$c3*$c3/4 - 2*$c2;
+                    if ($t1+$t2 >= $this->tolerance) {
+                        $d = sqrt($t1+$t2);
+
+                        $results[] = -$c3/4 + $d/2;
+                        $results[] = -$c3/4 - $d/2;
+                    }
+                    if ($t1-$t2 >= $this->tolerance) {
+                        $d = sqrt($t1-$t2);
+
+                        $results[] = -$c3/4 + $d/2;
+                        $results[] = -$c3/4 - $d/2;
+                    }
+                }
+            }
+        }
+
+        return $results;
+    }
+
+    /**
      * Gets roots of a cubic polynomial
      *
      * @return array $results Array of roots

tests/patterns/TestPatternTest.php
@@ -149,6 +149,22 @@ class TestPatternTest extends \PHPUnit\Framework\TestCase
     }
 
     /** 
+     * Tests the clonePoints method
+     */
+    public function testClonePoints()
+    {
+        $pattern = new \Freesewing\Patterns\TestPattern();
+        $p = $pattern->parts['testPart'];
+        $p->newPoint(1,2,3,'Just another point');
+        $p->addPoint(2, $p->shift(1,0,10),'A shifted point');
+        $pattern->addPart('part2');
+        $pattern->clonePoints('testPart','part2');
+        $this->assertEquals($pattern->parts['part2']->points[2]->getX(), 12);
+        $this->assertEquals($pattern->parts['part2']->points[2]->getY(), 3);
+        $this->assertEquals($pattern->parts['part2']->points[2]->getDescription(), 'A shifted point');
+    }
+
+    /** 
      * Tests messaging and debug
      */
     public function testMessagesAndDebug()

tests/src/GrowningPackerTest.php
@@ -89,7 +89,6 @@ class GrowingPackerTest extends \PHPUnit\Framework\TestCase
 
         $packer = new \Freesewing\Growingpacker();
         $packer->fit($layout); 
-        $this->saveTemplate(12,serialize($layout));
         $this->assertEquals(serialize($layout), $this->loadTemplate(12));
     }
 

tests/src/Part.offsetCode.Test.php
@@ -4,22 +4,6 @@ namespace Freesewing\Tests;
 
 class PartOffsetCode extends \PHPUnit\Framework\TestCase
 {
-    public function setUp()
-    {
-        //$this->startTime = time();
-    }
-
-    public function tearDown()
-    {
-        //$this->time('at teardown');
-    }
-
-    private function time($s='')
-    {
-        //$this->now = time();
-        //echo "\n".(time() - $this->startTime)." seconds: ".$s;
-    }
-    
     /**
      * Test offset of a line
      */
@@ -145,7 +129,6 @@ class PartOffsetCode extends \PHPUnit\Framework\TestCase
         $p->offsetPathString(1,'M 1 L 2 C 3 4 4', 10); // Inside offset, no gap
         $p->offsetPathString(2,'M 1 L 2 C 3 4 4', -10); // Outside offset, gap, cp2 = end
         $p->offsetPathString(3,'M 1 L 2 C 2 3 4', -10); // Outside offset, gap, start = cp1
-        $this->saveFixture('offsetLineCurve',serialize($p->paths));
         $this->assertEquals(serialize($p->paths),$this->loadFixture('offsetLineCurve'));
     }
 
@@ -163,7 +146,6 @@ class PartOffsetCode extends \PHPUnit\Framework\TestCase
         $p->newPoint(1,0,100);
         $p->offsetPathString(2,'M 4 C 4 3 2 L 1', 10); // Outside offset, gap, cp2 = end
         $p->offsetPathString(3,'M 4 C 3 2 2 L 1', 10); // Outside offset, gap, start = cp1
-        $this->saveFixture('offsetCurveLine',serialize($p->paths));
         $this->assertEquals(serialize($p->paths),$this->loadFixture('offsetCurveLine'));
     }
 
@@ -208,8 +190,6 @@ class PartOffsetCode extends \PHPUnit\Framework\TestCase
 
     private function saveFixture($fixture, $data)
     {
-        // use as 
-        //$this->saveFixture('grainline',serialize($p->dimensions));
         $dir = 'tests/src/fixtures';
         $file = "$dir/Part.offset.$fixture.data";
         $f = fopen($file,'w');

tests/src/PartTest.php
@@ -537,6 +537,46 @@ class PartTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($p->angle(1,7),315);
     }
 
+    /** 
+     * Tests the shiftTowards method
+     */
+    public function testShiftTowards()
+    {
+        $p = new \Freesewing\Part();
+        $p->newPoint(0,50,50);
+        $p->newPoint(1,0,0);
+        $p->newPoint(2,100,0);
+        $p->newPoint(3,100,100);
+        $p->newPoint(4,0,100);
+
+        $p->addPoint(10,$p->shiftTowards(0,1,40));
+        $p->addPoint(11,$p->shiftTowards(0,2,40));
+        $p->addPoint(12,$p->shiftTowards(0,3,40));
+        $p->addPoint(13,$p->shiftTowards(0,4,40));
+        $p->addPoint(14,$p->shiftTowards(1,2,40));
+        $p->addPoint(15,$p->shiftTowards(2,1,40));
+        $p->addPoint(16,$p->shiftTowards(1,4,40));
+        $p->addPoint(17,$p->shiftTowards(4,1,40));
+        
+        $this->assertEquals($p->points[10]->getX(),21.716);
+        $this->assertEquals($p->points[11]->getX(),78.284);
+        $this->assertEquals($p->points[12]->getX(),78.284);
+        $this->assertEquals($p->points[13]->getX(),21.716);
+        $this->assertEquals($p->points[14]->getX(),40);
+        $this->assertEquals($p->points[15]->getX(),60);
+        $this->assertEquals($p->points[16]->getX(),0);
+        $this->assertEquals($p->points[17]->getX(),0);
+        
+        $this->assertEquals($p->points[10]->getY(),21.716);
+        $this->assertEquals($p->points[11]->getY(),21.716);
+        $this->assertEquals($p->points[12]->getY(),78.284);
+        $this->assertEquals($p->points[13]->getY(),78.284);
+        $this->assertEquals($p->points[14]->getY(),0);
+        $this->assertEquals($p->points[15]->getY(),0);
+        $this->assertEquals($p->points[16]->getY(),40);
+        $this->assertEquals($p->points[17]->getY(),60);
+    }
+
     private function loadFixture($fixture)
     {
         $dir = 'tests/src/fixtures';
@@ -546,8 +586,6 @@ class PartTest extends \PHPUnit\Framework\TestCase
 
     private function saveFixture($fixture, $data)
     {
-        // use as 
-        //$this->saveFixture('grainline',serialize($p->dimensions));
         $dir = 'tests/src/fixtures';
         $file = "$dir/Part.$fixture.data";
         $f = fopen($file,'w');

tests/src/PolynomialTest.php
@@ -25,6 +25,27 @@ class PolynomialTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * Tests the getRoots method
+     */
+    public function testGetRoots()
+    {
+        $p = new \Freesewing\Polynomial([]);
+        $this->assertEquals($p->getRoots(), []);
+        
+        $p = new \Freesewing\Polynomial([1]);
+        $this->assertEquals($p->getRoots(), []);
+        
+        $p = new \Freesewing\Polynomial([1,2]);
+        $this->assertEquals($p->getRoots(), [-1]);
+        
+        $p = new \Freesewing\Polynomial([1,2,-3]);
+        $this->assertEquals($p->getRoots(), [1,-3]);
+        
+        $p = new \Freesewing\Polynomial([1,2,-3,4]);
+        $this->assertEquals($p->getRoots(), [-3.284277537307]);
+    }
+
+    /**
      * Tests exception thrown in evalu method
      *
      * @expectedException InvalidArgumentException

tests/src/SamplerTest.php
@@ -187,7 +187,6 @@ class SamplerTest extends \PHPUnit\Framework\TestCase
 
         $object->setPattern($pattern);
         $object->addSampledPartsToPattern();
-        $this->saveFixture('parts',serialize($object));
         $this->assertEquals(serialize($object), $this->loadFixture('parts'));
     }
     

commit e5b86ffd71538c7077d1a6d76d1c219cdfd677f4

Fixed Part::shiftTowards, broken by c6d2b897d7650b2a37947654972ad636b44f4f20

src/Part/Part.php
@@ -1827,12 +1827,34 @@ class Part
         $point1 = $this->loadPoint($key1);
         $point2 = $this->loadPoint($key2);
         $angle = $this->angle($key1, $key2);
-        
+
+        // cos is x axis, sin is y axis
+        $deltaX = $distance * abs(cos(deg2rad($angle)));
+        $deltaY = $distance * abs(sin(deg2rad($angle)));
+        if ($point1->getX() < $point2->getX() && $point1->getY() > $point2->getY()) {
+            $x = $point1->getX() + abs($deltaX);
+            $y = $point1->getY() - abs($deltaY);
+        } elseif ($point1->getX() < $point2->getX() && $point1->getY() < $point2->getY()) {
+            $x = $point1->getX() + abs($deltaX);
+            $y = $point1->getY() + abs($deltaY);
+        } elseif ($point1->getX() > $point2->getX() && $point1->getY() > $point2->getY()) {
+            $x = $point1->getX() - abs($deltaX);
+            $y = $point1->getY() - abs($deltaY);
+        } elseif ($point1->getX() > $point2->getX() && $point1->getY() < $point2->getY()) {
+            $x = $point1->getX() - abs($deltaX);
+            $y = $point1->getY() + abs($deltaY);
+        } else {
+            $x = $point1->getX() + $deltaX;
+            $y = $point1->getY() + $deltaY;
+        }
+
+        /*
         $deltaX = $distance * cos(deg2rad($angle)) * -1;
         $deltaY = $distance * sin(deg2rad($angle));
     
         $x = $point1->getX() + $deltaX;
         $y = $point1->getY() + $deltaY;
+         */
     
         return $this->createPoint($x, $y, "Point $key1 shifted towards $key2 by $distance");
     }
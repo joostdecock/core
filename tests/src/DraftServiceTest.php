7c13e04b167f82c4bd228c5f2e5bf4752223315e
Joost De Cock <joost@decock.org>
2017-03-14 16:16:42 +0000

    Fixed typo in DraftService tests

--- tests/src/DraftServiceTest.php
+++ tests/src/DraftServiceTest.php
@@ -49,7 +49,7 @@ class DraftServiceTest extends \PHPUnit\Framework\TestCase
     public function testRunBasicWithViewbox()
     {
         $context = new \Freesewing\Context();
-        $context->setRequest(new \Freesewing\Request(['service' => 'draft', 'pattern' => 'TestPattern', 'viewbox' => '10,10,200,200', 'theme' => 'paperless']));
+        $context->setRequest(new \Freesewing\Request(['service' => 'draft', 'pattern' => 'TestPattern', 'viewbox' => '10,10,200,200', 'theme' => 'Paperless']));
         $context->configure();
 
         $service = new DraftService();

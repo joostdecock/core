34fa4823611c7f55a53f7c733b8411bfb5704dd8
Joost De Cock <joost@decock.org>
2017-03-15 19:27:17 +0000

    Travis deploy script v1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-mkdir build
-mv * build
-tar -czf freesewing.tgz build
-export SSHPASS=$DEPLOY_PASS
-sshpass -e scp freesewing.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/develop/builds
-sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/develop/scripts/deploy.sh
+echo "Starting deploy script"
+if [ "$TRAVIS_PULL_REQUEST" ]; then
+    echo "Pull request, not deploying.";
+    exit
+else
+    if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ]; then
+        cd $TRAVIS_BUILD_DIR
+        mkdir build
+        mv * build
+        tar -czf freesewing.tgz build
+        export SSHPASS=$DEPLOY_PASS
+        sshpass -e scp freesewing.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/$TRAVIS_BRANCH/builds
+        sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/$TRAVIS_BRANCH/scripts/deploy.sh
+    else
+        echo "Branch is neither master nor develop, not deploying."
+    fi
+fi
+echo "Bye"

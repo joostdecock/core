a3294061824b3e2578fa9eeb0ca7fb1f0a551fb5
Joost De Cock <joost@decock.org>
2017-03-15 20:38:26 +0000

    Travis deploy script for auto-deploy over PHP 7.1 builds

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,12 @@ else
             echo "Deploying PHP version $TRAVIS_PHP_VERSION build.";
             cd $TRAVIS_BUILD_DIR
             mkdir build
-            echo $TRAVIS_PHP_VERSION
-            ls
             mv channels src themes patterns vendor index.php config.yml build/
             tar -czf freesewing.tgz build
             export SSHPASS=$DEPLOY_PASS
             sshpass -e scp -o stricthostkeychecking=no freesewing.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/$TRAVIS_BRANCH/builds
             sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST "cd /fs/deploy/core/$TRAVIS_BRANCH/builds ; tar -xzf freesewing.tgz ; rm freesewing.tgz ; rm -rf previous ; mv current previous ; mv build current "
+            echo "All done.";
         else
             echo "Build on PHP version $TRAVIS_PHP_VERSION, not deploying.";
         fi

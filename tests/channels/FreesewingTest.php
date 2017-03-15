7f1d8a774d7b174cadd41815cd1527a9e8effdf1
Joost De Cock <joost@decock.org>
2017-03-15 08:03:01 +0000

    Fixed patterns unit tests

--- tests/channels/FreesewingTest.php
+++ tests/channels/FreesewingTest.php
@@ -10,11 +10,11 @@ class FreesewingTest extends \PHPUnit\Framework\TestCase
         $context->setRequest(new \Freesewing\Request(['service' => 'info']));
         $context->configure();
         $channel = new \Freesewing\Channels\Core\Freesewing();
-        $this->assertEquals($channel->isValidrequest($context), false);
+        $this->assertEquals($channel->isValidrequest($context), true);
         
         $context = new \Freesewing\Context();
         $context->setRequest(new \Freesewing\Request(['service' => 'info', 'pattern' => 'TestPattern']));
-        $context->setPattern(new \Freesewing\Patterns\Tests\TestPattern()); 
+        $context->configure();
         $this->assertEquals($channel->isValidrequest($context), true);
 
     }
@@ -31,6 +31,7 @@ class FreesewingTest extends \PHPUnit\Framework\TestCase
     {
         $channel = new \Freesewing\Channels\Core\Freesewing();
         $pattern = new \Freesewing\Patterns\Tests\TestPattern();
+        $pattern->setUnits(['in' => 'metric']);
         $request = new \Freesewing\Request(['someMeasurement' => 97]);
         $measurements = $channel->standardizeModelMeasurements($request,$pattern);
         $this->assertEquals($measurements['someMeasurement'], 970);
@@ -42,12 +43,14 @@ class FreesewingTest extends \PHPUnit\Framework\TestCase
     {
         $channel = new \Freesewing\Channels\Core\Freesewing();
         $pattern = new \Freesewing\Patterns\Tests\TestPattern();
-        $request = new \Freesewing\Request(['measureOption' => 5, 'chooseOneOption' => 2]);
+        $pattern->setUnits(['in' => 'metric']);
+        $request = new \Freesewing\Request(['measureOption' => 4, 'chooseOneOption' => 2]);
         $options = $channel->standardizePatternOptions($request,$pattern);
-        $this->assertEquals($options['measureOption'], 50);
+        $this->assertEquals($options['measureOption'], 40);
         $this->assertEquals($options['chooseOneOption'], 2);
 
         $pattern = new \Freesewing\Patterns\Tests\TestPattern();
+        $pattern->setUnits(['in' => 'metric']);
         $request = new \Freesewing\Request(['percentOption' => 95]);
         $options = $channel->standardizePatternOptions($request,$pattern);
         $this->assertEquals($options['percentOption'], 0.95);

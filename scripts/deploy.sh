31ecd94daa89b3d4d81d6db4025110826c826fce
Joost De Cock <joost@decock.org>
2017-03-15 20:12:00 +0000

    Travis deploy script v5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,11 +7,11 @@ else
     if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ]; then
         cd $TRAVIS_BUILD_DIR
         mkdir build
-        cp -R index.php channels patterns src themes patterns vendor index.php config.yml build/
+        mv channels patterns src themes patterns vendor index.php config.yml build/
         tar -czf freesewing.tgz build
         export SSHPASS=$DEPLOY_PASS
         sshpass -e scp -o stricthostkeychecking=no freesewing.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/$TRAVIS_BRANCH/builds
-        sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/$TRAVIS_BRANCH/scripts/deploy.sh
+        sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST "cd /fs/deploy/core/$TRAVIS_BRANCH/builds ; tar -xzf freesewing.tgz ; rm freesewing.tgz ; rm -rf previous ; mv current previous ; mv build current "
     else
         echo "Branch is neither master nor develop, not deploying."
     fi

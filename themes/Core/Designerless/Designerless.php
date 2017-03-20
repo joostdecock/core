1dad859bbac485793cc8bc68e9c57c91893d6151
Joost De Cock <joost@decock.org>
2017-03-20 14:31:42 +0000

    Made the designer theme non-paperless
    And I've added the Designerless theme, which is a
    paperless designer theme (get it?)

---
+++ themes/Core/Designerless/Designerless.php
@@ -0,0 +1,14 @@
+<?php
+/** Freesewing\Themes\Core\Designerless class */
+namespace Freesewing\Themes\Core;
+
+/**
+ * This is the designer theme, with paperless set to true in the config
+ * 
+ * In other words, this shows the paperless info too.
+ *
+ * @author Joost De Cock <joost@decock.org>
+ * @copyright 2016 Joost De Cock
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, Version 3
+ */
+class Designerless extends Designer { }

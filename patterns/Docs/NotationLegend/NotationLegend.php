5aafec3d5c32d37b6e569354c3c97a0d6090979c
Joost De Cock <joost@decock.org>
2017-08-22 09:02:36 +0000

    Added NotationLegend pattern for documentation

--- patterns/Docs/NotationLegend/NotationLegend.php
+++ patterns/Docs/NotationLegend/NotationLegend.php
@@ -118,4 +118,294 @@ class NotationLegend extends \Freesewing\Patterns\Core\Pattern
         }
         $this->addBox($p,150,470);
     }
+    
+    /**
+     * helpLine
+     */
+    private function example_helpLine($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-11", $p->shift("$type-4",-90,50));
+            $p->newPoint("$type-12", $p->x("$type-1"), $p->y("$type-11"));
+            $p->newPath("$type-help","M $type-11 L $type-12", ['class' => 'help']);   
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * hintLine
+     */
+    private function example_hintLine($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-11", $p->shift("$type-5",210,10));
+            $p->addPoint("$type-12", $p->shift("$type-11",180,20));
+            $p->addPoint("$type-13", $p->shift("$type-11",-90,20));
+            $p->addPoint("$type-14", $p->shift("$type-12",-90,20));
+            $p->newPoint("$type-15", $p->x("$type-14")+10, $p->y("$type-14")+3);
+            $p->newPath("$type-help","M $type-11 L $type-13 L $type-15 L $type-14 L $type-12 z", ['class' => 'hint']);   
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * grainLine
+     */
+    private function example_grainLine($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-11", $p->shift("$type-8",-90,10));
+            $p->newPoint("$type-12", $p->x("$type-11"), $p->y("$type-2")-5);
+            $p->newGrainline("$type-12","$type-11", 'Grainline');
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * Cut-on-fold line, cofLine
+     */
+    private function example_cofLine($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-11", $p->shift("$type-1",-90,10));
+            $p->addPoint("$type-12", $p->shift("$type-2",90,10));
+            $p->newCutOnFold("$type-12","$type-11", 'Cut on fold', 13);
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * Notches
+     */
+    private function example_notches($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-11", $p->curveEdge("$type-4", "$type-5", "$type-6", "$type-7", 'left'));
+            $p->notch(["$type-11"]);
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * Buttons
+     */
+    private function example_buttons($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-11", $p->shiftFractionTowards("$type-1", "$type-2", 0.2));
+            $p->addPoint("$type-12", $p->shiftFractionTowards("$type-1", "$type-2", 0.4));
+            $p->addPoint("$type-13", $p->shiftFractionTowards("$type-1", "$type-2", 0.6));
+            $p->addPoint("$type-14", $p->shiftFractionTowards("$type-1", "$type-2", 0.8));
+            $p->addPoint("$type-11", $p->shift("$type-11", 0, 8));
+            $p->addPoint("$type-12", $p->shift("$type-12", 0, 8));
+            $p->addPoint("$type-13", $p->shift("$type-13", 0, 8));
+            $p->addPoint("$type-14", $p->shift("$type-14", 0, 8));
+            $p->newSnippet("$type-btn1", 'button', "$type-11");
+            $p->newSnippet("$type-btn2", 'button', "$type-12");
+            $p->newSnippet("$type-btn3", 'button', "$type-13");
+            $p->newSnippet("$type-btn4", 'button', "$type-14");
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * Buttonholess
+     */
+    private function example_buttonholes($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-11", $p->shiftFractionTowards("$type-1", "$type-2", 0.2));
+            $p->addPoint("$type-12", $p->shiftFractionTowards("$type-1", "$type-2", 0.4));
+            $p->addPoint("$type-13", $p->shiftFractionTowards("$type-1", "$type-2", 0.6));
+            $p->addPoint("$type-14", $p->shiftFractionTowards("$type-1", "$type-2", 0.8));
+            $p->addPoint("$type-11", $p->shift("$type-11", 0, 8));
+            $p->addPoint("$type-12", $p->shift("$type-12", 0, 8));
+            $p->addPoint("$type-13", $p->shift("$type-13", 0, 8));
+            $p->addPoint("$type-14", $p->shift("$type-14", 0, 8));
+            $p->newSnippet("$type-btn1", 'buttonhole', "$type-11");
+            $p->newSnippet("$type-btn2", 'buttonhole', "$type-12");
+            $p->newSnippet("$type-btn3", 'buttonhole', "$type-13");
+            $p->newSnippet("$type-btn4", 'buttonhole', "$type-14");
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * Buttonholess
+     */
+    private function example_snaps($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-13", $p->shiftFractionTowards("$type-1", "$type-2", 0.8));
+            $p->addPoint("$type-14", $p->shiftFractionTowards("$type-1", "$type-2", 0.95));
+            $p->addPoint("$type-11", $p->shift("$type-13", 0, 72));
+            $p->addPoint("$type-12", $p->shift("$type-14", 0, 72));
+            $p->addPoint("$type-13", $p->shift("$type-13", 0, 8));
+            $p->addPoint("$type-14", $p->shift("$type-14", 0, 8));
+            $p->newSnippet("$type-btn1", 'snap-female', "$type-11");
+            $p->newSnippet("$type-btn2", 'snap-male', "$type-12");
+            $p->newSnippet("$type-btn3", 'snap-female', "$type-13");
+            $p->newSnippet("$type-btn4", 'snap-male', "$type-14");
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * Logo
+     */
+    private function example_logo($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+
+        $this->garment($p, 0  ,5,'fabric');
+        $this->garment($p, 90 ,5,'lining');
+        $this->garment($p, 180,5,'interfacing');
+        $this->garment($p, 270,5,'canvas');
+        $this->garment($p, 360,5,'various');
+
+        $types = ['fabric','lining','interfacing','canvas','various'];
+        foreach($types as $type) {
+            $p->newPath($type,"M $type-1 L $type-2 L $type-3 L $type-4 C $type-5 $type-6 $type-7 L $type-8 C $type-9 $type-10 $type-1 z", ['class' => $type]);   
+            $p->newTextOnPath($type, "M $type-2 L $type-3", $type, ['dy' => -5, 'class' => "text-lg"], false);
+            $p->addPoint("$type-11", $p->shiftFractionTowards("$type-1", "$type-2", 0.5));
+            $p->addPoint("$type-11", $p->shift("$type-11", 0, 35));
+            $p->newSnippet("$type-logo", 'logo', "$type-11");
+        }
+        $this->addBox($p,150,470);
+    }
+    
+    /**
+     * Scalebox
+     */
+    private function example_scalebox($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+        $p->newPoint (1, 50, 5);
+        $p->newSnippet('box', 'scalebox', 1);
+        $this->addBox($p,60,110);
+        $this->replace('__REFERENCE__','abcde');
+    }
+    
+    /**
+     * Title
+     */
+    private function example_title($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+        $p->newPoint (1, 20, 40);
+        $p->addTitle('1', 1,'Front', 'Cut one');
+        $this->addBox($p,60,110);
+    }
+    
+    /**
+     * Note
+     */
+    private function example_note($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+        $p->newPoint (1, 10, 10);
+        $p->newNote('1', 1,'I am a note');
+        $this->addBox($p,30,110);
+    }
+    
+    /**
+     * Dimension
+     */
+    private function example_dimension($p, $model)
+    {
+        /** @var \Freesewing\Part $p */
+        $p->newPoint (1, 10, 20);
+        $p->newPoint (2, 110, 20);
+        $p->newLinearDimension(1,2);
+        $this->addBox($p,30,110);
+    }
+    
 }

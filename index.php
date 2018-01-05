45ef10d8572ceaa1ecea55af567500a12b142456
Joost De Cock <joost@decock.org>
2018-01-05 08:30:07 +0000

    Added rollbar integration

--- index.php
+++ index.php
@@ -20,4 +20,17 @@ $context = new \Freesewing\Context();
 $context->setRequest(new \Freesewing\Request($_REQUEST));
 $context->configure();
 
+// Rollbar integration
+if($context->getConfig()['integrations']['rollbar'] !== false) {
+//    use \Rollbar\Rollbar;
+  //  use \Rollbar\Payload\Level;
+    \Rollbar\Rollbar::init([
+        'access_token' => getenv('ROLLBAR_ACCESS_TOKEN'),
+        'environment' => getenv('ROLLBAR_ENVIRONMENT'),
+    ]);
+
+    \Rollbar\Rollbar::log(\Rollbar\Payload\Level::info(), 'Test info message');
+    throw new Exception('Test exception');
+}
+
 $context->runService();
